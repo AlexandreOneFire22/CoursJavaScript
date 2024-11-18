73a6916eb1623cc8b4a8a908c625efeae5abf584
GAUTHIER Alexandre <alexgau70@gmail.com>
2024-11-18 09:49:23 +0100

    :)

--- intro/variable.js
+++ intro/variable.js
@@ -1,7 +1,59 @@
 // Les variables en JavaScript
 
-console.log("Cours JavaScript");
+console.log("Cours JavaScript")
 
 //Déclaration d'une variable
 
-let a=10;
+let a=10
+
+console.log(a)
+
+a=20
+
+console.log(a)
+
+let b
+
+console.log(b) // Undefined
+
+// Déclaration constante
+
+const c = 10
+
+console.log(c)
+
+//c = 20 -> Erreur Constante (on peut pas la modfier)
+// const d -> Erreur (Obligé d'être assigné)
+
+//Déclaration d'une chaine de caractère
+
+let promotion = "BTS SIO2"
+console.log(promotion)
+
+//Immutabilité des chaînes
+let prenom = "john"
+console.log(prenom)
+
+prenom[0] = "J"
+console.log(prenom)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
